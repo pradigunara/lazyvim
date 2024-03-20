6d7671fcca51163f2cde8a9238d57220f139c631
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2024-03-20 08:52:03 +0800

    config overrides

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- normal with jk
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
+vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>") -- exit to normal terminal
+vim.keymap.set("n", "<A-v>", "<C-v>") -- visual block alias
