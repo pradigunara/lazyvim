96dea04681dfcc3b89bf0aa6426d29f7777e3e14
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2024-03-20 08:52:03 +0800

    config overrides

---
+++ lua/plugins/lang.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+      servers = {
+        gopls = { -- https://www.lazyvim.org/extras/lang/go
+          settings = {
+            gopls = {
+              analyses = {
+                fieldalignment = false,
+              },
+            },
+          },
+        },
+        eslint = {},
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "vtsls" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+}
