dcfe016ededb860bf7edbb49d70e541deafdf2f5
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2025-01-24 07:51:36 +0700

    use deepseek for avante

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -15,9 +15,18 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "openai",
+      provider = "deepseek",
       openai = {
         model = "gpt-4o-mini",
+      },
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-chat",
+          temperature = 0,
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
