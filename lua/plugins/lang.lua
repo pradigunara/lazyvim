42dc09b2bfa9bcdc9b4874311320692a5b4a480b
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2025-02-18 10:18:18 +0700

    test comment out unnecessary lang config

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -25,31 +25,31 @@ return {
             },
           },
         },
-        eslint = {},
-        jsonls = { -- copied directly from extras
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-          end,
-          settings = {
-            json = {
-              format = { enable = true },
-              validate = { enable = true },
-            },
-          },
-        },
+        -- eslint = {},
+        -- jsonls = { -- copied directly from extras
+        --   -- lazy-load schemastore when needed
+        --   on_new_config = function(new_config)
+        --     new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+        --     vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+        --   end,
+        --   settings = {
+        --     json = {
+        --       format = { enable = true },
+        --       validate = { enable = true },
+        --     },
+        --   },
+        -- },
       },
       setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "vtsls" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
+        -- eslint = function()
+        --   require("lazyvim.util").lsp.on_attach(function(client)
+        --     if client.name == "eslint" then
+        --       client.server_capabilities.documentFormattingProvider = true
+        --     elseif client.name == "vtsls" then
+        --       client.server_capabilities.documentFormattingProvider = false
+        --     end
+        --   end)
+        -- end,
       },
     },
   },
