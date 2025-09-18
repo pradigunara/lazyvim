f901ae7b1f076c407d80f909350f08bee2421bc0
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2025-09-18 17:10:58 +0700

    update to lazyvim 15

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,26 +7,27 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("custom-go-autocmd", { clear = true }),
-  pattern = { "*.go" },
-  callback = function()
-    vim.cmd("silent setlocal ts=4 sw=4")
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   group = vim.api.nvim_create_augroup("custom-go-autocmd", { clear = true }),
+--   pattern = { "*.go" },
+--   callback = function()
+--     vim.cmd("silent setlocal ts=4 sw=4")
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+--   group = vim.api.nvim_create_augroup("custom-hl-insert", { clear = true }),
+--   pattern = { "*" },
+--   callback = function()
+--     vim.cmd("silent setlocal cursorline")
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "BufRead" }, {
+--   group = vim.api.nvim_create_augroup("custom-nohl-normal", { clear = true }),
+--   pattern = { "*" },
+--   callback = function()
+--     vim.cmd("silent setlocal nocursorline")
+--   end,
+-- })
 
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-  group = vim.api.nvim_create_augroup("custom-hl-insert", { clear = true }),
-  pattern = { "*" },
-  callback = function()
-    vim.cmd("silent setlocal cursorline")
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave", "BufRead" }, {
-  group = vim.api.nvim_create_augroup("custom-nohl-normal", { clear = true }),
-  pattern = { "*" },
-  callback = function()
-    vim.cmd("silent setlocal nocursorline")
-  end,
-})
